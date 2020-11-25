175e2f619c96b543b528652826c7cfe7f89b1721
Raminta <56090053+ramintaquiet@users.noreply.github.com>
2020-11-25 16:54:47 +0000

    app set up, nav bar, custom bootstrap

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  root 'pages#home'
+
+ 
+  get '/home', to: 'pages#home'
+  get '/about', to: 'pages#about'
+  get '/info', to: 'pages#info'
+  get '/pets', to: 'pages#pets'
+  get  '/favorite', to:'pages#favorite'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
